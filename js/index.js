62d782ef5bd963e06e1ffbe8c5bdf5cc3b11fee3
Arcadio REYES <arcadioreyesph@gmail.com>
2022-10-20 12:31:47 +0200

    Added platforms, cars, cars' speed and gaps

--- js/index.js
+++ js/index.js
@@ -10,10 +10,13 @@ background.src = "Imagesbicycle/background1.png";
 let playerImage = new Image();
 playerImage.src = "Imagesbicycle/alleyCat.png";
 
+let carImage = new Image();
+carImage.src = "Imagesbicycle/car.png";
+
 const gravity = 1;
 
 class Player {
-  constructor() {
+  constructor(cars) {
     this.position = {
       x: 100,
       y: 100,
@@ -25,6 +28,17 @@ class Player {
 
     this.width = 70;
     this.height = 90;
+    this.carSpawnTime = 2;
+    this.cars = cars;
+    this.lives = 3;
+    // this.isHit = 0;
+
+    // displaying cars every 4 seconds
+    setInterval(() => {
+      this.cars.push(
+        new Car({ x: canvas.width, y: 540 - 75, image: carImage })
+      );
+    }, this.carSpawnTime * 1000);
   }
 
   draw() {
@@ -45,7 +59,7 @@ class Player {
 
     if (this.position.y + this.height + this.velocity.y <= canvas.height)
       this.velocity.y += gravity;
-    // else this.velocity.y = 0;
+    else this.velocity.y = 0;
   }
 }
 
@@ -67,17 +81,64 @@ class Platform {
   }
 }
 
+// creation car and speed
+class Car {
+  constructor({ x, y, image }) {
+    this.position = {
+      x: x,
+      y: y,
+    };
+
+    this.image = image;
+
+    this.width = image.width / 2;
+    this.height = image.height / 2;
+    this.speed = 6;
+  }
+
+  draw() {
+    c.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.image.width / 3,
+      this.image.height / 3
+    );
+  }
+
+  update() {
+    this.position.x -= this.speed;
+  }
+}
+
+// adding platform trees
 const image = new Image();
-image.src = "Imagesbicycle/troncoImage.png";
+image.src = "Imagesbicycle/3troncos.png";
 
 console.log(image);
 
-const player = new Player();
+let cars = [new Car({ x: canvas.width, y: 540 - 75, image: carImage })];
+
+const player = new Player(cars);
+
+// adding 1st level of platform trees
 const platforms = [
-  new Platform({ x: -10, y: 470, image: image }),
-  new Platform({ x: image.width - 3, y: 470, image: image }),
-  new Platform({ x: image.width * 2 + 100, y: 470, image: image }),
-  new Platform({ x: image.width * 3 + 300, y: 470, image: image }),
+  new Platform({ x: -10, y: 530, image: image }),
+  new Platform({ x: image.width - 3, y: 530, image: image }),
+  new Platform({ x: image.width * 2, y: 530, image: image }),
+  new Platform({ x: image.width * 3, y: 530, image: image }),
+  new Platform({ x: image.width * 4, y: 530, image: image }),
+  new Platform({ x: image.width * 5, y: 530, image: image }),
+  new Platform({ x: image.width * 6, y: 530, image: image }),
+
+  // adding 2nd level of platform trees
+  new Platform({ x: -10, y: 370, image: image }),
+  new Platform({ x: image.width * 1.1, y: 340, image: image }),
+  new Platform({ x: image.width * 2.3, y: 210, image: image }),
+  new Platform({ x: image.width * 2.8, y: 370, image: image }), //
+  new Platform({ x: image.width * 4, y: 310, image: image }),
+  new Platform({ x: image.width * 4.6, y: 200, image: image }),
+  new Platform({ x: image.width * 6, y: 370, image: image }),
 ];
 
 const keys = {
@@ -92,6 +153,10 @@ const keys = {
 let scrollOffset = 0;
 
 function animate() {
+  if (player.lives <= 0) {
+    console.log("game over");
+  }
+
   requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
@@ -102,7 +167,15 @@ function animate() {
     platform.draw();
   });
 
+  cars.forEach((car) => {
+    car.draw();
+  });
+
   player.update();
+
+  cars.forEach((car) => {
+    car.update();
+  });
 
   if (keys.right.pressed && player.position.x < 400) {
     player.velocity.x = 5;
@@ -111,15 +184,23 @@ function animate() {
   } else {
     player.velocity.x = 0;
 
+    // movement platform
     if (keys.right.pressed) {
       scrollOffset += 5;
       platforms.forEach((platform) => {
         platform.position.x -= 5;
       });
+
+      cars.forEach((car) => {
+        car.position.x -= 5;
+      });
     } else if (keys.left.pressed) {
       scrollOffset -= 5;
       platforms.forEach((platform) => {
         platform.position.x += 5;
+      });
+      cars.forEach((car) => {
+        car.position.x += 5;
       });
     }
   }
@@ -137,19 +218,35 @@ function animate() {
     }
   });
 
+  // car collision detection
+  cars.forEach((car) => {
+    if (
+      player.position.y + player.height >= car.position.y &&
+      player.position.y <= car.position.y + car.height &&
+      player.position.x + player.width >= car.position.x &&
+      player.position.x <= car.position.x + car.width
+    ) {
+      console.log("hit");
+      // player.isHit += 1;
+
+      // if (player.isHit === 1) {
+      //   player.lives -= 1;
+
+      // }
+    } //else if (player.position.x > car.position.x && car.canHit) {
+    //   player.isHit = 0;
+    // }
+  });
+
   // WIN CONDITION & you win message
-  if (scrollOffset > 2000) {
+  if (scrollOffset > image.width * 6) {
     console.log("You win!");
   }
 }
 
-// LOSE CONDITION IT DOES NOT WORK 1'27
-if (player.position.y > canvas.height) {
-  console.log("You lose!");
-}
-
 animate();
 
+// Controlling the character with arrows
 window.addEventListener("keydown", ({ keyCode }) => {
   console.log(keyCode);
   switch (keyCode) {
@@ -193,7 +290,7 @@ window.addEventListener("keyup", ({ keyCode }) => {
 
     case 38:
       console.log("up");
-      player.velocity.y -= 20;
+      player.velocity.y -= 15;
       break;
   }
 });
